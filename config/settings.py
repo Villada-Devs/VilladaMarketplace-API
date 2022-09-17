1376477c6c0ad0d4ed669139c0d1c1cf5e9eb5ff
marcosdon28 <marcosdon28@gmail.com>
2022-09-16 22:42:11 -0300

    update database config

--- config/settings.py
+++ config/settings.py
@@ -149,10 +149,17 @@ Database config
 """
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': os.environ['DATABASE_NAME'] , 
+        'USER': os.environ['DATABASE_USER'],  
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],  
+        'HOST': os.environ['DATABASE_HOST'],  
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }     
+    }  
 }
 
 
