56fc3647859c7d9a6dd0b4b7697968b845b2149e
marcosdon28 <marcosdon28@gmail.com>
2022-09-29 12:42:26 -0300

    arreglado !

--- events/serializers.py
+++ events/serializers.py
@@ -17,7 +17,7 @@ class EventsSerializer(serializers.ModelSerializer):
     #Campo para ver las imagenes que tiene el objeto(solo lectura)
     imagesevent = ImagesEventSerializer(many=True, read_only =True)
     #Campo para mandar imagenes al objeto(solo escritura)
-    uploaded_images = serializers.ListField(child = serializers.FileField(max_length = 1000000, allow_empty_file = False, use_url = False), write_only = True, required=False)
+    uploaded_images = serializers.ListField(child = serializers.ImageField(max_length = 1000000, allow_empty_file = False, use_url = False), write_only = True, required=False)
     class Meta:
         model = Event
         fields = ['id',
@@ -32,24 +32,28 @@ class EventsSerializer(serializers.ModelSerializer):
         ]
     
     def create(self, validated_data):
-        if validated_data.get('uploaded_images'):
+        try: 
             uploaded_data = validated_data.pop('uploaded_images')
             event = Event.objects.create(**validated_data)
 
             for uploaded_item in uploaded_data:
                 ImagesEvent.objects.create(event=event, image= uploaded_item)
-            return event
-        return validated_data
-    
+            
+        except Exception as ex:
+            raise serializers.ValidationError({'error ' : 'no se puede crear un evento sin una imagen'})
+        return event
+        
+    #Este metodo se usa para borrar las imagenes 
     def clear_existing_images(self, instance):
         for event_image in instance.imagesevent.all():
             event_image.delete()
-
+    
 
     def update(self, instance, validated_data):
         images = validated_data.pop('uploaded_images', None)
         if images:
-            self.clear_existing_images(instance)
+            #esta linea de codigo borra las imagenes existentes y carga las nuevas que se mandan en el PUT
+            #self.clear_existing_images(instance)
             event_image_model_instance = [ImagesEvent(event=instance, image=image)for image in images]
             ImagesEvent.objects.bulk_create(
                 event_image_model_instance
