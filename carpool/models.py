a60e741f287ca5bc95e7faf10c65db8c5d991ab0
marcosdon28 <marcosdon28@gmail.com>
2022-09-29 12:28:45 -0300

    arreglar algunas cositas de las imagenes

--- carpool/models.py
+++ carpool/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-
-
 class Pool(models.Model):
     created_by = models.ForeignKey(User, on_delete= models.CASCADE)
     locality = models.CharField(max_length=70)
